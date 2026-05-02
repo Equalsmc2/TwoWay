02756e78f5e1458fbe8b0a6e6e23b8791c17e15a
Equalsmc2 <155104649+Equalsmc2@users.noreply.github.com>
2026-05-01 21:50:20 -0400

    Add 'mirror_seeker' to teamAmber and connections

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const db = getDatabase(app);
 // Define which characters belong to which reality (use all lowercase)
 // Example: Adding "new_seeker" to Azure and "shadow_seeker" to Amber
 const teamAzure = ["azure_seeker","weaveshaper_seeker", "fabled_seeker", "typed_seeker", "faded_seeker", "mneme_seeker"]; 
-const teamAmber = ["amber_seeker","old_seeker", "prince_seeker", "written_seeker", "dual_seeker"];
+const teamAmber = ["amber_seeker","old_seeker", "prince_seeker", "written_seeker", "dual_seeker", "mirror_seeker];
 
 // Define the 1-on-1 connections across the rift
 const characterPairs = {
@@ -39,8 +39,8 @@ const characterPairs = {
     "dual_seeker": "faded_seeker",
     "faded_seeker": "dual_seeker" 
 
-    //"new_seeker": "mneme_seeker",
-    //"mneme_seeker": "new_seeker" 
+    "mirror_seeker": "mneme_seeker",
+    "mneme_seeker": "mirror_seeker" 
 };
 
 // --- UI ELEMENTS ---
